89b5bf2b6eff17182bf01e0ae94f0d7196f2d537
JaQuan Banks <shadic151@gmail.com>
2022-10-12 17:01:55 -0400

    commit tree 155347c47bd66ee4505d2b854c4b628d2ff52ced

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(greeting){
+  if (greeting <= 400) {
+    greeting = "This one is on me!"
+    return greeting
+  } else if (greeting <= 2000) {
+    greeting = "That will be twenty bucks."
+    return greeting
+  } else if (greeting <= 2499) {
+    greeting = "I will gladly take your thirty bucks."
+    return greeting
+  } else if (greeting >= 2500) {
+    greeting = "No can do."
+    return greeting
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const response = city === "NYC" ? "Ok, sounds good." : "No go."
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case tip = "generous":
+      var charm = "Thank you so much."
+      return charm;
+    case tip = "not as generous":
+      var charm = "Thank you."
+      return charm;
+
+    default:
+      var charm = "Bye."
+      return charm;
+      break;
+  }
 }
